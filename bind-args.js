dc723159534ad688489d46104cc921e5ff84393f
kerryChen95 <95kerry@gmail.com>
2014-11-26 02:23:33 +0800

    Fix bug: `arguments` can not be concerted as array

--- bind-args.js
+++ bind-args.js
@@ -1,8 +1,10 @@
 module.exports = bindArgs
 
 function bindArgs (func) {
-  var boundArgs = [].slice.call(arguments, 1)
+  var slice = [].slice
+  var boundArgs = slice.call(arguments, 1)
   return function () {
-    func.apply(this, boundArgs.concat(arguments))
+    var args = slice.call(arguments)
+    func.apply(this, boundArgs.concat(args))
   }
 }
